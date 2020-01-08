fac7fd452dc9fa591e1f69052bb15a70b9da0190
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-01-08 23:10:29 +0530

    TS basics

---
+++ TS/index.js
@@ -0,0 +1,14 @@
+/*
+
+    Typescript is a superset of JS.
+    It is a strongly typed language which provides effecient tooling 🛠(The TSC) to detect bugs at compile time.
+    This helps us to write more predictable code.
+    There is no learning curve as every JS code is valid in TS ╰(*°▽°*)╯
+    It helps us to write code with future JS features, so stays always updated!!
+
+    The TSC compiler by-default converts any TS code to ES3 🙆🏼‍♂️
+*/
+console.log('Hello World!');
+async function hello() {
+    return '🍱🍱🍱';
+}
